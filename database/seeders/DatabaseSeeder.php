f69de424599b362cfe6c796a1935f1a32c843a23
gustisvastika <general.swastika@gmail.com>
2023-08-29 20:25:41 +0800

    just fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -39,44 +39,44 @@ class DatabaseSeeder extends Seeder
             'administrator' => false,
         ]);
 
-        // Areas
-        foreach ([1, 2] as $item) {
-            Area::create([
-                'name' => 'Area ' . $item,
-            ]);
-        };
+        // // Areas
+        // foreach ([1, 2] as $item) {
+        //     Area::create([
+        //         'name' => 'Area ' . $item,
+        //     ]);
+        // };
 
-        $areas = Area::all();
+        // $areas = Area::all();
 
-        // Vets
-        for ($i = 1; $i <= 10; $i++) {
-            $vet = new Vet();
-            $vet->user_id = $admin->id;
-            $vet->area_id = $areas->random()->id;
-            $vet->name = 'Vet ' . $i;
-            $vet->telephone = '123456789';
-            $vet->whatsapp = '123456789';
-            $vet->save();
+        // // Vets
+        // for ($i = 1; $i <= 10; $i++) {
+        //     $vet = new Vet();
+        //     $vet->user_id = $admin->id;
+        //     $vet->area_id = $areas->random()->id;
+        //     $vet->name = 'Vet ' . $i;
+        //     $vet->telephone = '123456789';
+        //     $vet->whatsapp = '123456789';
+        //     $vet->save();
             
-            // Membuat jadwal untuk Vet
-            $days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+        //     // Membuat jadwal untuk Vet
+        //     $days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
             
-            foreach ($days as $day) {
-                $schedule = [
-                    'open_day' => true, // Ganti dengan logika validasi Anda
-                    'open_hour' => '09:00',
-                    'close_hour' => '17:00',
-                    'fullday' => false, // Ganti dengan logika validasi Anda
-                ];
+        //     foreach ($days as $day) {
+        //         $schedule = [
+        //             'open_day' => true, // Ganti dengan logika validasi Anda
+        //             'open_hour' => '09:00',
+        //             'close_hour' => '17:00',
+        //             'fullday' => false, // Ganti dengan logika validasi Anda
+        //         ];
                 
-                $scheduleModel = new Schedule();
-                $scheduleModel->vet_id = $vet->id;
-                $scheduleModel->day_name = $day;
-                $scheduleModel->open_hour = $schedule['open_hour'];
-                $scheduleModel->close_hour = $schedule['close_hour'];
-                $scheduleModel->fullday = $schedule['fullday'];
-                $scheduleModel->save();
-            }
-        }
+        //         $scheduleModel = new Schedule();
+        //         $scheduleModel->vet_id = $vet->id;
+        //         $scheduleModel->day_name = $day;
+        //         $scheduleModel->open_hour = $schedule['open_hour'];
+        //         $scheduleModel->close_hour = $schedule['close_hour'];
+        //         $scheduleModel->fullday = $schedule['fullday'];
+        //         $scheduleModel->save();
+        //     }
+        // }
     }
 }
